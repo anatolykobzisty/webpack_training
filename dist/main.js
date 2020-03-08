85bc60aad2df564adb9e57978543ad6a884a5fb6
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-03-08 20:03:38 +0600

    21_Refactoring_webpack.config

---
+++ dist/main.js
@@ -0,0 +1,249 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// install a JSONP callback for chunk loading
+/******/ 	function webpackJsonpCallback(data) {
+/******/ 		var chunkIds = data[0];
+/******/ 		var moreModules = data[1];
+/******/ 		var executeModules = data[2];
+/******/
+/******/ 		// add "moreModules" to the modules object,
+/******/ 		// then flag all "chunkIds" as loaded and fire callback
+/******/ 		var moduleId, chunkId, i = 0, resolves = [];
+/******/ 		for(;i < chunkIds.length; i++) {
+/******/ 			chunkId = chunkIds[i];
+/******/ 			if(Object.prototype.hasOwnProperty.call(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 				resolves.push(installedChunks[chunkId][0]);
+/******/ 			}
+/******/ 			installedChunks[chunkId] = 0;
+/******/ 		}
+/******/ 		for(moduleId in moreModules) {
+/******/ 			if(Object.prototype.hasOwnProperty.call(moreModules, moduleId)) {
+/******/ 				modules[moduleId] = moreModules[moduleId];
+/******/ 			}
+/******/ 		}
+/******/ 		if(parentJsonpFunction) parentJsonpFunction(data);
+/******/
+/******/ 		while(resolves.length) {
+/******/ 			resolves.shift()();
+/******/ 		}
+/******/
+/******/ 		// add entry modules from loaded chunk to deferred list
+/******/ 		deferredModules.push.apply(deferredModules, executeModules || []);
+/******/
+/******/ 		// run deferred modules when all chunks ready
+/******/ 		return checkDeferredModules();
+/******/ 	};
+/******/ 	function checkDeferredModules() {
+/******/ 		var result;
+/******/ 		for(var i = 0; i < deferredModules.length; i++) {
+/******/ 			var deferredModule = deferredModules[i];
+/******/ 			var fulfilled = true;
+/******/ 			for(var j = 1; j < deferredModule.length; j++) {
+/******/ 				var depId = deferredModule[j];
+/******/ 				if(installedChunks[depId] !== 0) fulfilled = false;
+/******/ 			}
+/******/ 			if(fulfilled) {
+/******/ 				deferredModules.splice(i--, 1);
+/******/ 				result = __webpack_require__(__webpack_require__.s = deferredModule[0]);
+/******/ 			}
+/******/ 		}
+/******/
+/******/ 		return result;
+/******/ 	}
+/******/
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// object to store loaded and loading chunks
+/******/ 	// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 	// Promise = chunk loading, 0 = chunk loaded
+/******/ 	var installedChunks = {
+/******/ 		"main": 0
+/******/ 	};
+/******/
+/******/ 	var deferredModules = [];
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
+/******/ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
+/******/ 	jsonpArray.push = webpackJsonpCallback;
+/******/ 	jsonpArray = jsonpArray.slice();
+/******/ 	for(var i = 0; i < jsonpArray.length; i++) webpackJsonpCallback(jsonpArray[i]);
+/******/ 	var parentJsonpFunction = oldJsonpFunction;
+/******/
+/******/
+/******/ 	// add entry module to deferred list
+/******/ 	deferredModules.push(["./index.js","vendors~analytics~main"]);
+/******/ 	// run deferred modules when ready
+/******/ 	return checkDeferredModules();
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./assets/data.csv":
+/*!*************************!*\
+  !*** ./assets/data.csv ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = [[\"Transaction_date\",\"Product\",\"Price\",\"Payment_Type\",\"Name\",\"City\",\"State\",\"Country\",\"Account_Created\",\"Last_Login\",\"Latitude\",\"Longitude\"],[\"1/2/09 6:17\",\"Product1\",\"1200\",\"Mastercard\",\"carolina\",\"Basildon\",\"England\",\"United Kingdom\",\"1/2/09 6:00\",\"1/2/09 6:08\",\"51.5\",\"-1.1166667\"],[\"1/2/09 4:53\",\"Product1\",\"1200\",\"Visa\",\"Betina\",\"Parkville                   \",\"MO\",\"United States\",\"1/2/09 4:42\",\"1/2/09 7:49\",\"39.195\",\"-94.68194\"],[\"1/2/09 13:08\",\"Product1\",\"1200\",\"Mastercard\",\"Federica e Andrea\",\"Astoria                     \",\"OR\",\"United States\",\"1/1/09 16:21\",\"1/3/09 12:32\",\"46.18806\",\"-123.83\"],[\"1/3/09 14:44\",\"Product1\",\"1200\",\"Visa\",\"Gouya\",\"Echuca\",\"Victoria\",\"Australia\",\"9/25/05 21:13\",\"1/3/09 14:22\",\"-36.1333333\",\"144.75\"],[\"1/4/09 12:56\",\"Product2\",\"3600\",\"Visa\",\"Gerd W \",\"Cahaba Heights              \",\"AL\",\"United States\",\"11/15/08 15:47\",\"1/4/09 12:45\",\"33.52056\",\"-86.8025\"],[\"1/4/09 13:19\",\"Product1\",\"1200\",\"Visa\",\"LAURENCE\",\"Mickleton                   \",\"NJ\",\"United States\",\"9/24/08 15:19\",\"1/4/09 13:04\",\"39.79\",\"-75.23806\"],[\"1/4/09 20:11\",\"Product1\",\"1200\",\"Mastercard\",\"Fleur\",\"Peoria                      \",\"IL\",\"United States\",\"1/3/09 9:38\",\"1/4/09 19:45\",\"40.69361\",\"-89.58889\"],[\"1/2/09 20:09\",\"Product1\",\"1200\",\"Mastercard\",\"adam\",\"Martin                      \",\"TN\",\"United States\",\"1/2/09 17:43\",\"1/4/09 20:01\",\"36.34333\",\"-88.85028\"],[\"1/4/09 13:17\",\"Product1\",\"1200\",\"Mastercard\",\"Renee Elisabeth\",\"Tel Aviv\",\"Tel Aviv\",\"Israel\",\"1/4/09 13:03\",\"1/4/09 22:10\",\"32.0666667\",\"34.7666667\"],[\"1/4/09 14:11\",\"Product1\",\"1200\",\"Visa\",\"Aidan\",\"Chatou\",\"Ile-de-France\",\"France\",\"6/3/08 4:22\",\"1/5/09 1:17\",\"48.8833333\",\"2.15\"],[\"1/5/09 2:42\",\"Product1\",\"1200\",\"Diners\",\"Stacy\",\"New York                    \",\"NY\",\"United States\",\"1/5/09 2:23\",\"1/5/09 4:59\",\"40.71417\",\"-74.00639\"],[\"1/5/09 5:39\",\"Product1\",\"1200\",\"Amex\",\"Heidi\",\"Eindhoven\",\"Noord-Brabant\",\"Netherlands\",\"1/5/09 4:55\",\"1/5/09 8:15\",\"51.45\",\"5.4666667\"],[\"1/2/09 9:16\",\"Product1\",\"1200\",\"Mastercard\",\"Sean \",\"Shavano Park                \",\"TX\",\"United States\",\"1/2/09 8:32\",\"1/5/09 9:05\",\"29.42389\",\"-98.49333\"],[\"1/5/09 10:08\",\"Product1\",\"1200\",\"Visa\",\"Georgia\",\"Eagle                       \",\"ID\",\"United States\",\"11/11/08 15:53\",\"1/5/09 10:05\",\"43.69556\",\"-116.35306\"],[\"1/2/09 14:18\",\"Product1\",\"1200\",\"Visa\",\"Richard\",\"Riverside                   \",\"NJ\",\"United States\",\"12/9/08 12:07\",\"1/5/09 11:01\",\"40.03222\",\"-74.95778\"],[\"1/4/09 1:05\",\"Product1\",\"1200\",\"Diners\",\"Leanne\",\"Julianstown\",\"Meath\",\"Ireland\",\"1/4/09 0:00\",\"1/5/09 13:36\",\"53.6772222\",\"-6.3191667\"],[\"1/5/09 11:37\",\"Product1\",\"1200\",\"Visa\",\"Janet\",\"Ottawa\",\"Ontario\",\"Canada\",\"1/5/09 9:35\",\"1/5/09 19:24\",\"45.4166667\",\"-75.7\"],[\"1/6/09 5:02\",\"Product1\",\"1200\",\"Diners\",\"barbara\",\"Hyderabad\",\"Andhra Pradesh\",\"India\",\"1/6/09 2:41\",\"1/6/09 7:52\",\"17.3833333\",\"78.4666667\"],[\"1/6/09 7:45\",\"Product2\",\"3600\",\"Visa\",\"Sabine\",\"London\",\"England\",\"United Kingdom\",\"1/6/09 7:00\",\"1/6/09 9:17\",\"51.52721\",\"0.14559\"],[\"1/2/09 7:35\",\"Product1\",\"1200\",\"Diners\",\"Hani\",\"Salt Lake City              \",\"UT\",\"United States\",\"12/30/08 5:44\",\"1/6/09 10:52\",\"40.76083\",\"-111.89028\"],[\"1/6/09 12:56\",\"Product1\",\"1200\",\"Visa\",\"Jeremy\",\"Manchester\",\"England\",\"United Kingdom\",\"1/6/09 10:58\",\"1/6/09 13:31\",\"53.5\",\"-2.2166667\"],[\"1/1/09 11:05\",\"Product1\",\"1200\",\"Diners\",\"Janis\",\"Ballynora\",\"Cork\",\"Ireland\",\"12/10/07 12:37\",\"1/7/09 1:52\",\"51.8630556\",\"-8.58\"],[\"1/5/09 4:10\",\"Product1\",\"1200\",\"Mastercard\",\"Nicola\",\"Roodepoort\",\"Gauteng\",\"South Africa\",\"1/5/09 2:33\",\"1/7/09 5:13\",\"-26.1666667\",\"27.8666667\"],[\"1/6/09 7:18\",\"Product1\",\"1200\",\"Visa\",\"asuman\",\"Chula Vista                 \",\"CA\",\"United States\",\"1/6/09 7:07\",\"1/7/09 7:08\",\"32.64\",\"-117.08333\"],[\"1/2/09 1:11\",\"Product1\",\"1200\",\"Mastercard\",\"Lena\",\"Kuopio\",\"Ita-Suomen Laani\",\"Finland\",\"12/31/08 2:48\",\"1/7/09 10:20\",\"62.9\",\"27.6833333\"],[\"1/1/09 2:24\",\"Product1\",\"1200\",\"Visa\",\"Lisa\",\"Sugar Land                  \",\"TX\",\"United States\",\"1/1/09 1:56\",\"1/7/09 10:52\",\"29.61944\",\"-95.63472\"],[\"1/7/09 8:08\",\"Product1\",\"1200\",\"Diners\",\"Bryan Kerrene\",\"New York                    \",\"NY\",\"United States\",\"1/7/09 7:39\",\"1/7/09 12:38\",\"40.71417\",\"-74.00639\"],[\"1/2/09 2:57\",\"Product1\",\"1200\",\"Visa\",\"chris\",\"London\",\"England\",\"United Kingdom\",\"1/3/08 7:23\",\"1/7/09 13:14\",\"51.52721\",\"0.14559\"],[\"1/1/09 20:21\",\"Product1\",\"1200\",\"Visa\",\"Maxine\",\"Morton                      \",\"IL\",\"United States\",\"10/24/08 6:48\",\"1/7/09 20:49\",\"40.61278\",\"-89.45917\"],[\"1/8/09 0:42\",\"Product1\",\"1200\",\"Visa\",\"Family\",\"Los Gatos                   \",\"CA\",\"United States\",\"1/8/09 0:28\",\"1/8/09 3:39\",\"37.22667\",\"-121.97361\"],[\"1/8/09 3:56\",\"Product1\",\"1200\",\"Mastercard\",\"Katherine\",\"New York                    \",\"NY\",\"United States\",\"1/8/09 3:33\",\"1/8/09 6:19\",\"40.71417\",\"-74.00639\"],[\"1/8/09 3:16\",\"Product1\",\"1200\",\"Mastercard\",\"Linda\",\"Miami                       \",\"FL\",\"United States\",\"1/8/09 3:06\",\"1/8/09 6:34\",\"25.77389\",\"-80.19389\"],[\"1/8/09 1:59\",\"Product1\",\"1200\",\"Mastercard\",\"SYLVIA\",\"Vesenaz\",\"Geneve\",\"Switzerland\",\"11/28/07 11:56\",\"1/8/09 7:20\",\"46.2333333\",\"6.2\"],[\"1/3/09 9:03\",\"Product1\",\"1200\",\"Diners\",\"Sheila\",\"Brooklyn                    \",\"NY\",\"United States\",\"1/3/09 8:47\",\"1/8/09 10:38\",\"40.65\",\"-73.95\"],[\"1/5/09 13:17\",\"Product1\",\"1200\",\"Mastercard\",\"Stephanie\",\"Badhoevedorp\",\"Noord-Holland\",\"Netherlands\",\"1/5/09 12:45\",\"1/8/09 11:45\",\"52.3333333\",\"4.7833333\"],[\"1/6/09 7:46\",\"Product1\",\"1200\",\"Amex\",\"Kelly\",\"Reston                      \",\"VA\",\"United States\",\"1/6/09 7:30\",\"1/8/09 12:40\",\"38.96861\",\"-77.34139\"],[\"1/5/09 20:00\",\"Product2\",\"3600\",\"Visa\",\"James\",\"Burpengary\",\"Queensland\",\"Australia\",\"12/10/08 19:53\",\"1/8/09 17:58\",\"-27.1666667\",\"152.95\"],[\"1/8/09 16:24\",\"Product1\",\"1200\",\"Visa\",\"jennifer\",\"Phoenix                     \",\"AZ\",\"United States\",\"1/8/09 15:57\",\"1/8/09 18:30\",\"33.44833\",\"-112.07333\"],[\"1/9/09 6:39\",\"Product1\",\"1200\",\"Mastercard\",\"Anneli\",\"Houston                     \",\"TX\",\"United States\",\"1/9/09 5:09\",\"1/9/09 7:11\",\"29.76306\",\"-95.36306\"],[\"1/6/09 22:19\",\"Product2\",\"3600\",\"Amex\",\"Ritz\",\"Pittsfield                  \",\"VT\",\"United States\",\"1/6/09 12:00\",\"1/9/09 10:05\",\"43.77222\",\"-72.81333\"],[\"1/6/09 23:00\",\"Product2\",\"3600\",\"Amex\",\"Sylvia\",\"Pittsfield                  \",\"VT\",\"United States\",\"1/6/09 12:00\",\"1/9/09 10:05\",\"43.77222\",\"-72.81333\"],[\"1/7/09 7:44\",\"Product1\",\"1200\",\"Mastercard\",\"Marie\",\"Ball Ground                 \",\"GA\",\"United States\",\"1/7/09 5:35\",\"1/9/09 10:52\",\"34.33806\",\"-84.37667\"]]\n\n//# sourceURL=webpack:///./assets/data.csv?");
+
+/***/ }),
+
+/***/ "./assets/data.xml":
+/*!*************************!*\
+  !*** ./assets/data.xml ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = {\"email\":{\"to\":[\"Vladilen\"],\"from\":[\"Webpack\"],\"heading\":[\"Tutorial\"],\"body\":[\"Finish the record\"]}}\n\n//# sourceURL=webpack:///./assets/data.xml?");
+
+/***/ }),
+
+/***/ "./assets/json.json":
+/*!**************************!*\
+  !*** ./assets/json.json ***!
+  \**************************/
+/*! exports provided: title, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"title\\\":\\\"I am JSON title\\\"}\");\n\n//# sourceURL=webpack:///./assets/json.json?");
+
+/***/ }),
+
+/***/ "./assets/webpack-logo.png":
+/*!*********************************!*\
+  !*** ./assets/webpack-logo.png ***!
+  \*********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"c500a3801d8356a86da86a06c3d13a4d.png\");\n\n//# sourceURL=webpack:///./assets/webpack-logo.png?");
+
+/***/ }),
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! jquery */ \"../node_modules/jquery/dist/jquery.js\");\n/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _models_Post__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @models/Post */ \"./models/Post.js\");\n/* harmony import */ var _styles_styles_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @/styles/styles.css */ \"./styles/styles.css\");\n/* harmony import */ var _styles_styles_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_styles_styles_css__WEBPACK_IMPORTED_MODULE_2__);\n/* harmony import */ var _styles_less_less__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./styles/less.less */ \"./styles/less.less\");\n/* harmony import */ var _styles_less_less__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_styles_less_less__WEBPACK_IMPORTED_MODULE_3__);\n/* harmony import */ var _assets_webpack_logo__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./assets/webpack-logo */ \"./assets/webpack-logo.png\");\n/* harmony import */ var _assets_json__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./assets/json */ \"./assets/json.json\");\nvar _assets_json__WEBPACK_IMPORTED_MODULE_5___namespace = /*#__PURE__*/__webpack_require__.t(/*! ./assets/json */ \"./assets/json.json\", 1);\n/* harmony import */ var _assets_data_xml__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./assets/data.xml */ \"./assets/data.xml\");\n/* harmony import */ var _assets_data_xml__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(_assets_data_xml__WEBPACK_IMPORTED_MODULE_6__);\n/* harmony import */ var _assets_data_csv__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./assets/data.csv */ \"./assets/data.csv\");\n/* harmony import */ var _assets_data_csv__WEBPACK_IMPORTED_MODULE_7___default = /*#__PURE__*/__webpack_require__.n(_assets_data_csv__WEBPACK_IMPORTED_MODULE_7__);\n\n\n\n\n\n\n\n\n\n\n\nconst post = new _models_Post__WEBPACK_IMPORTED_MODULE_1__[\"default\"]('Webpack', _assets_webpack_logo__WEBPACK_IMPORTED_MODULE_4__[\"default\"]);\n\njquery__WEBPACK_IMPORTED_MODULE_0__('pre')\n  .addClass('code')\n  .html(post.toString());\n\n// console.log(json);\n// console.log(xml);\n// console.log(csv);\n\n\n//# sourceURL=webpack:///./index.js?");
+
+/***/ }),
+
+/***/ "./models/Post.js":
+/*!************************!*\
+  !*** ./models/Post.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return Post; });\nclass Post {\n  constructor(title, image) {\n    this.title = title;\n    this.image = image;\n    this.date = new Date();\n  }\n\n  toString() {\n    return JSON.stringify(\n      {\n        title: this.title,\n        date: this.date.toJSON(),\n        image: this.image,\n      },\n      null,\n      2\n    );\n  }\n\n  get uppercaseTitle() {\n    return this.toUppercase();\n  }\n}\n\n\n//# sourceURL=webpack:///./models/Post.js?");
+
+/***/ }),
+
+/***/ "./styles/less.less":
+/*!**************************!*\
+  !*** ./styles/less.less ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n    if(false) { var cssReload; }\n  \n\n//# sourceURL=webpack:///./styles/less.less?");
+
+/***/ }),
+
+/***/ "./styles/styles.css":
+/*!***************************!*\
+  !*** ./styles/styles.css ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n    if(false) { var cssReload; }\n  \n\n//# sourceURL=webpack:///./styles/styles.css?");
+
+/***/ })
+
+/******/ });
