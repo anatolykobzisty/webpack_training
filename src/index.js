082ee80ca917c5cd6525818a159097b2f9fb2038
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-03-08 16:10:51 +0600

    07_JSON

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Post from './Post';
 import './styles/styles.css';
+import json from './assets/json';
 
 const post = new Post({ title: 'Hello Webpack !' });
 
 console.log(post.toString());
+
+console.log(json);
