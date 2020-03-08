4c98c0ffc08727491c6ffc2ad031c4676e86fa94
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-03-08 16:23:39 +0600

    10_CSV-loader

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './styles/styles.css';
 import WebpackLogo from './assets/webpack-logo.png';
 import json from './assets/json';
 import xml from './assets/data.xml';
+import csv from './assets/data.csv';
 
 const post = new Post('Webpack', WebpackLogo);
 
@@ -10,3 +11,4 @@ console.log(post.toString());
 
 console.log(json);
 console.log(xml);
+console.log(csv);
