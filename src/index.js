5d0470ea06fa981a5d8256d9f92de8cc10c4e0c5
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-03-08 19:51:43 +0600

    20_Less

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import * as $ from 'jquery';
 
 import Post from '@models/Post';
 import '@/styles/styles.css';
+import './styles/less.less';
+
 import WebpackLogo from './assets/webpack-logo';
 import json from './assets/json';
 import xml from './assets/data.xml';
