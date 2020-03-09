19aaa3929683e8e7d719214d79848af4b4854a7f
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-03-09 14:22:19 +0600

    18_TerserWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,24 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all',
+    },
+  };
+
+  if (isProd) {
+    config.minimizer = [new TerserWebpackPlugin()];
+  }
+
+  return config;
+};
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -25,11 +40,7 @@ module.exports = {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
+  optimization: optimization(),
   devServer: {
     port: 4200,
     hot: isDev,
