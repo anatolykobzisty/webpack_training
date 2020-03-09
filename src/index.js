99b06f9ee0e63e9d275ed8dcc745fa67006a0a53
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-03-09 13:53:48 +0600

    09_XML-loader

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import Post from './Post';
 import './styles/styles.css';
 import WebpackLogo from './assets/webpack-logo.png';
 import json from './assets/json';
+import xml from './assets/data.xml';
 
 const post = new Post('Webpack', WebpackLogo);
 
 console.log(post.toString());
 
 console.log(json);
+console.log(xml);
