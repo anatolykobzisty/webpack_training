33673d67d92255ac1ddaa17f764fbdd0a1132f58
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-03-09 13:18:19 +0600

    03_CleanWebpackPugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -15,5 +16,6 @@ module.exports = {
     new HTMLWebpackPlugin({
       template: './src/index.html',
     }),
+    new CleanWebpackPlugin(),
   ],
 };
