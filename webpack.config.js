e1615b4c23e89cf9ceb3b84e8f88467ac253c03f
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-03-09 14:09:27 +0600

    11_Setting_resolve

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,13 @@ module.exports = {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  resolve: {
+    extensions: ['.js', '.json', '.png'],
+    alias: {
+      '@models': path.resolve(__dirname, 'src/models'),
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   plugins: [
     new HTMLWebpackPlugin({
       template: './index.html',
